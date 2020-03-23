512a230c0e3ebabf74c27eaf4d147c5d2537e3b5
ye fan <yefan@startdt.com>
2020-03-23 19:40:38 +0800

    提交表结构

---
+++ docs/sql/short_url.sql
@@ -0,0 +1,35 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50720
+ Source Host           : localhost
+ Source Database       : short_url
+
+ Target Server Type    : MySQL
+ Target Server Version : 50720
+ File Encoding         : utf-8
+
+ Date: 03/23/2020 19:40:05 PM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `tb_short_url`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_short_url`;
+CREATE TABLE `tb_short_url` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `hash_value` varchar(32) NOT NULL,
+  `url` varchar(2048) NOT NULL,
+  `created` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `yn` tinyint(2) DEFAULT '0' COMMENT '是否删除 1 删除 0 未删除',
+  PRIMARY KEY (`id`),
+  KEY `unique` (`hash_value`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
